857e137978ec13c280932e82c5cf394edcb2ace9
guest <mmanikar@kanuradh-mac-0.lan>
2020-09-27 11:32:03 -0400

    Updates

---
+++ precache-manifest.e4363bd649ec5de44a6c5cc72e6265c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341ebf92d6c79e0815100c4bce0a730c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "564733708a629124f455",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564733708a629124f455",
+    "url": "/tic-tac-toe/static/js/main.9bf53a04.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
